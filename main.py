0315c3960d13657111ee57cfb59f349a8e40eac9
vicenteh2o <vicentefleitas@Vicentes-MacBook-Pro.local>
2025-10-27 15:05:29 -0300

    chore: response model

--- main.py
+++ main.py
@@ -14,11 +14,11 @@ def create_item(item: Item):
     items.append(item)
     return items
 
-@app.get("/items")
+@app.get("/items", response_model=list[Item])
 def list_items(limit: int = 10):
     return items[0:limit]
 
-@app.get("/items/{item_id}") 
+@app.get("/items/{item_id}", response_model=Item)
 def get_item(item_id: int) -> Item:
     if item_id < len(items):
         return items[item_id]
